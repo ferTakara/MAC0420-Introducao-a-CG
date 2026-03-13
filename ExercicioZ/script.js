dc3a6539fefbc8578eea4a609dd2160802d2e61d
fernando8534 <fernandotakara143@gmail.com>
2026-03-13 10:00:55 -0300

    terminado o exercicio z

--- ExercicioZ/script.js
+++ ExercicioZ/script.js
@@ -1,96 +1,73 @@
 window.onload = main
 
-var ctx;
-var interface = {
-    side: 'l',
-    size: 50    
-}
-
-var canvas;
-var lastEvent;
-var lado;
 function main() {
-    console.log("Starting");
-    initControl()
-    initCanvas()
-}
+    const canvas = document.getElementById('meucanvas')
+    const square = new Square(0, 0, 0, 'rgb(255, 0, 0)', canvas)    
+    canvas.addEventListener('click', (e) => square.update(e.offsetX, e.offsetY))    
+    document.getElementById("Botao_C").addEventListener('click', () => square.stepUp())
+    document.getElementById("Botao_B").addEventListener('click', () => square.stepDown())
+    document.getElementById("Botao_E").addEventListener('click', () => square.stepLeft())
+    document.getElementById("Botao_D").addEventListener('click', () => square.stepRight())
 
-function initControl() {
-    lado = document.getElementById('Lado')
-    lado.addEventListener('change', controlEventListener)
-    barra_r  = document.getElementById("Barra R")
-    barra_r .addEventListener('change', (e) => controlEventListener(e, 'r'))
-    barra_g  = document.getElementById("Barra G")
-    barra_g .addEventListener('change', (e) => controlEventListener(e, 'g'))
-    barra_b  = document.getElementById("Barra B")
-    barra_b .addEventListener('change', controlEventListener)
-    lado = document.getElementById("Lado")
-    lado.addEventListener('change', controlEventListener)
-    botao_c  = document.getElementById("Botao_C")
-    botao_c .addEventListener('click', controlEventListener)
-    botao_e  = document.getElementById("Botao_E")
-    botao_e .addEventListener('click', controlEventListener)
-    botao_d  = document.getElementById("Botao_D")
-    botao_d .addEventListener('click', controlEventListener)
-    botao_b = document.getElementById("Botao_B")
-    botao_b.addEventListener('click', controlEventListener)
-    passo  = document.getElementById("Passo")
-    passo .addEventListener('change', controlEventListener)
-}
-function controlEventListener(e, type) {
-    console.log(type, e.target.value)
-    let step = passo.value
-    if (type == ">"){
-        drawSquare(interface.coords.x + step, interface.coords.y)
-    }
-    if (type == "V"){
-        drawSquare(interface.coords.x, interface.coords.y + step)
-        
-    }
-    if (type == "<"){
-        drawSquare(interface.coords.x - step, interface.coords.y)
-        
-    }
-    if (type == "^"){
-        drawSquare(interface.coords.x, interface.coords.y - step)
-        
-    }
-    lastEvent = e
-}
-function initCanvas() {    
-    console.log("getting context")
-    canvas = document.getElementById('meucanvas')
-    ctx = canvas.getContext('2d')   
-    canvas.addEventListener('click', clickHandler)
-}
-
-
-function clickHandler(e) {
-    const coords = { 'x': e.offsetX, 'y': e.offsetY}
-    interface.coords = coords
-    console.log(`handling click on ${coords}`)    
-    drawSquare(coords.x, coords.y)
-}
-
-function drawSquare(x, y) {    
-    cleanCanvas()
-    let quad = new Path2D();
-    let r = barra_r.value
-    let g= barra_g.value
-    let b = barra_b.value
-    let size = parseInt(lado.value)
-    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`
+    document.getElementById("Barra R").addEventListener('change', () => square.update())
+    document.getElementById("Barra G").addEventListener('change', () => square.update())
+    document.getElementById("Barra B").addEventListener('change', () => square.update())
     
-    console.log(`drawing, ${x}, ${y},${size}`)
-    quad.moveTo( x, y);
-    quad.lineTo( x+size, y);
-    quad.lineTo( x+size, y+size);
-    quad.lineTo( x, y+size);
-    quad.closePath();
-    ctx.fill(quad);    
+    document.getElementById("Lado").addEventListener('change', () => square.update())
 }
 
-
-function cleanCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+class Square {
+    constructor(x, y, size, color, canvas) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.color = color
+        this.ctx = canvas.getContext('2d')
+        this.canvas = canvas
+    }
+    stepUp() {
+        this.step(0, -this.getPasso())
+    }
+    stepDown() {
+        this.step(0, this.getPasso())
+    }
+    stepLeft() {
+        this.step(-this.getPasso(), 0)
+    }
+    stepRight() {
+        this.step(this.getPasso(), 0)
+    }
+    step(xIncrement, yIncrement) {        
+        this.update(this.x+=xIncrement, this.y+=yIncrement)
+    }
+    update(x, y) {
+        this.x = x? x : this.x
+        this.y = y? y : this.y
+        this.size = this.getLado()
+        this.color = this.getColor()
+        this.clean()
+        this.draw()
+    }
+    clean() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+    draw() {
+        this.ctx.fillStyle = this.color
+        let quad = new Path2D();
+        quad.moveTo(this.x, this.y);
+        quad.lineTo(this.x + this.size, this.y);
+        quad.lineTo(this.x + this.size, this.y + this.size);
+        quad.lineTo(this.x, this.y + this.size);
+        quad.closePath();
+        this.ctx.fill(quad);
+    }
+    getLado() {
+        return parseInt(document.getElementById('Lado').value)
+    }
+    getPasso() {
+        return parseInt(document.getElementById('Passo').value)
+    }
+    getColor() {
+        return `rgb(${parseInt(document.getElementById('Barra R').value)}, ${parseInt(document.getElementById('Barra G').value)}, ${parseInt(document.getElementById('Barra B').value)})`
+    }
 }
